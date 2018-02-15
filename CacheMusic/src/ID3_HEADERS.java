92d6e8fb36cac0ad461c8480b21cd5235b32fddb
Artur <shalaev-artur@yandex.ru>
2018-02-15 18:44:15 +0300

    Added ID3 headers

---
+++ CacheMusic/src/ID3_HEADERS.java
@@ -0,0 +1,8 @@
+public class ID3_HEADERS {
+    public static final String ARTIST = "TPE1";
+    public static final String TITLE = "TIT2";
+    public static final String ALBUM = "TALB";
+    public static final String YEAR = "TYER";
+    public static final String COMMENT = "vCOMM";
+    public static final String GENRE = "TCON";
+}
