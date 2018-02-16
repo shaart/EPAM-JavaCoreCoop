999a3b73d0c1a9f1603c0506c52a75d03feb6870
Artur <shalaev-artur@yandex.ru>
2018-02-16 17:29:03 +0300

    Renamed ID3_HEADERS to Metadata

--- CacheMusic/src/Metadata.java
+++ CacheMusic/src/Metadata.java
@@ -1,4 +1,8 @@
-public class ID3_HEADERS {
+public class Metadata {
+    static enum Name { ID3v1, ID3v2 };
+    String Tag;
+    String Version;
+
     public static final String ARTIST = "TPE1";
     public static final String TITLE = "TIT2";
     public static final String ALBUM = "TALB";
