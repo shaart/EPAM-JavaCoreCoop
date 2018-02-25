65bbb89b0409ddedbf18f1b13440a76a6190ec5b
MatveyD <matv1995@gmail.com>
2018-02-25 05:26:33 +0300

    Zip + io (zip unzip) added

--- JCL/src/commands/Zip.java
+++ JCL/src/commands/Zip.java
@@ -8,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Zip implements Commandable {
@@ -33,8 +35,17 @@ public class Zip implements Commandable {
     @Override
     public void run(String args[]) {
         String compressionIndex = args[0];
-       // Path folderToZip = Paths.get(args[0]);
-        String fileName = args[1];
+        if (compressionIndex.trim().equals("a") ){
+            zip (args[1]);
+        }
+        if (compressionIndex.trim().equals("u") ){
+            unzip (args[1]);
+        }
+        // Path folderToZip = Paths.get(args[0]);
+    }
+
+    private void zip (String fileName){
+        //String fileName = args[1];
 
         File toZip = new File(fileName);
         //FileOutputStream fos = null;
@@ -53,7 +64,7 @@ public class Zip implements Commandable {
         System.out.println("file " + fileName + ".zip has been successfully created");
     }
 
-    private static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
+    private void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
         if (fileToZip.isHidden()) {
             return;
         }
@@ -61,6 +72,7 @@ public class Zip implements Commandable {
             File[] children = fileToZip.listFiles();
             for (File childFile : children != null ? children : new File[0]) {
                 zipFile(childFile, fileName + "\\" + childFile.getName(), zipOut);
+                System.out.println("file zipped : "+ childFile.getAbsoluteFile());
             }
             return;
         }
@@ -73,6 +85,40 @@ public class Zip implements Commandable {
             zipOut.write(bytes, 0, length);
         }
         fis.close();
+    }
+
+
+    private void unzip(String fileZip){
+        fileZip += ".zip";
+        String outputZip = fileZip.substring(0, fileZip.lastIndexOf("//"));
+
+        //System.out.println(outputZip);
+        //System.out.println(fileZip);
+
+        byte[] buffer = new byte[1024];
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip))) {
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                String fileName = zipEntry.getName();
+                File newFile = new File(outputZip + File.separator + fileName);
+
+                System.out.println("file unzip : "+ newFile.getAbsoluteFile());
+
+                //create all non exists folders
+                new File(newFile.getParent()).mkdirs();
+
+
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+                zipEntry = zis.getNextEntry();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
 
