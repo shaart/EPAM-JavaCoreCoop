56ef5b3bdee96d1bcae14902f56e069c337c09a9
Artur <imshaart@gmail.com>
2018-03-06 00:36:34 +0300

    Initial commit

---
+++ BookLibrary/src/main/java/javacore/coop/model/Book.java
@@ -0,0 +1,10 @@
+package javacore.coop.model;
+
+/**
+ * Book's model
+ */
+public class Book {
+    /* TODO
+        - Check and create Book's model
+     */
+}
---
+++ BookLibrary/src/main/java/javacore/coop/model/User.java
@@ -0,0 +1,10 @@
+package javacore.coop.model;
+
+/**
+ * User's model
+ */
+public class User {
+    /* TODO
+        - Check and create User's model
+     */
+}
