6aa2694af9bff3d24cc866e2174b219131cd571e
Artur <imshaart@gmail.com>
2018-03-12 16:25:47 +0300

    Added "IF NOT EXISTS", refactored tabs

--- BookLibrary/scripts/books.sql
+++ BookLibrary/scripts/books.sql
@@ -1,11 +1,11 @@
-CREATE TABLE BOOKS
+CREATE TABLE IF NOT EXISTS BOOKS
 (
-  ISBN            VARCHAR(2147483647)                                                                                                     NOT NULL,
-  ID              INTEGER DEFAULT 0 AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  ADDED_DATE      TIMESTAMP DEFAULT NOW()                                                                                                 NOT NULL,
+  ISBN            VARCHAR(2147483647)                           NOT NULL,
+  ID              INTEGER DEFAULT 0 AUTO_INCREMENT PRIMARY KEY  NOT NULL,
+  ADDED_DATE      TIMESTAMP DEFAULT NOW()                       NOT NULL,
   AUTHOR          VARCHAR(2147483647),
-  TITLE           VARCHAR(2147483647)                                                                                                     NOT NULL,
-  REMAINED_AMOUNT INTEGER DEFAULT 0                                                                                                       NOT NULL
+  TITLE           VARCHAR(2147483647)                           NOT NULL,
+  REMAINED_AMOUNT INTEGER DEFAULT 0                             NOT NULL
 );
 CREATE UNIQUE INDEX IF NOT EXISTS BOOKS_ISBN_UINDEX
   ON BOOKS (ISBN)
