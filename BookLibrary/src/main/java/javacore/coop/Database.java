2016c6769c4e8eec60b1cedc67a56c0878d18414
Artur <imshaart@gmail.com>
2018-03-12 15:47:19 +0300

    Fixed bugs, code refactoring, added exception throwing and documentation

--- BookLibrary/src/main/java/javacore/coop/Database.java
+++ BookLibrary/src/main/java/javacore/coop/Database.java
@@ -3,132 +3,179 @@ package javacore.coop;
 import javacore.coop.model.Book;
 import javacore.coop.model.BookNotFoundException;
 
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.sql.*;
-import java.time.LocalDateTime;
-import java.time.temporal.TemporalAccessor;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Layout for working with database
  */
-public class Database implements AutoCloseable{
-    /* TODO
-        - add necessary methods
-        - realize all methods
-     */
-
+public class Database implements AutoCloseable {
     public static final String JDBC_DRIVER = "org.h2.Driver";
 
     private static final String DB_PREFIX = "jdbc:h2:";
     public static final String DEFAULT_DB_URL = "./db/library";
     private static final String DB_POSTFIX = ";IFEXISTS=TRUE";
 
-    private static final String GET_ALL = "SELECT * FROM BOOKS";
-    String SORT_BY_SQL = GET_ALL + " order by "+"?";
-    String ADD_BOOK = "INSERT INTO BOOKS (TITLE, AUTHOR, ISBN, ADDED_DATE, REMAINED_AMOUNT) VALUES (?,?,?,?,?)";
-    String FIND_BOOK_ISBN = "SELECT ADDED_DATE,AUTHOR,TITLE,REMAINED_AMOUNT FROM BOOKS WHERE ISBN=?";
+    private static final String SELECT_ALL = "SELECT * FROM BOOKS ";
+    private static final String SELECT_ALL_ORDER_BY = SELECT_ALL + " order by ";
+    private static final String ADD_BOOK = "INSERT INTO BOOKS (TITLE, AUTHOR, ISBN, ADDED_DATE, " +
+            " REMAINED_AMOUNT) VALUES (?,?,?,?,?)";
+    private static final String FIND_BOOK_ISBN = "SELECT ADDED_DATE,AUTHOR,TITLE,REMAINED_AMOUNT " +
+            "FROM BOOKS WHERE ISBN=?";
 
-    //private final String DB_URL;
-    //private final String USER;
-    //private final char[] PASSWORD;
+    private Connection connection = null;
 
-    public Connection connection = null;
-
-    private Database (Connection connection){
-
-            this.connection = connection;
+    private Database(Connection connection) {
+        this.connection = connection;
     }
 
+    /**
+     * Create table "BOOKS" at database if not exists using <code>connection</code>.
+     *
+     * @param connection Connection to database
+     */
+    private static void createBooksTableIfNotExists(Connection connection) {
+        final String CREATE_BOOKS_TABLE_IF_NOT_EXIST = "CREATE TABLE IF NOT EXISTS BOOKS" +
+                "(" +
+                "  ID INTEGER DEFAULT 0 AUTO_INCREMENT PRIMARY KEY NOT NULL," +
+                "  ISBN VARCHAR(2147483647) NOT NULL," +
+                "  ADDED_DATE TIMESTAMP DEFAULT NOW() NOT NULL," +
+                "  AUTHOR VARCHAR(2147483647)," +
+                "  TITLE VARCHAR(2147483647) NOT NULL," +
+                "  REMAINED_AMOUNT INTEGER DEFAULT 0 NOT NULL" +
+                "); CREATE UNIQUE INDEX IF NOT EXISTS BOOKS_ISBN_UINDEX ON BOOKS (ISBN)";
+        try {
+            PreparedStatement prst = connection.prepareStatement(CREATE_BOOKS_TABLE_IF_NOT_EXIST);
+            boolean result = prst.execute();
+            System.out.println("Table \"BOOKS\" " + (result ? "successfully created" : "found"));
+        } catch (SQLException e) {
+            // Can't do anything
+            e.printStackTrace();
+        }
+    }
 
-    private static Connection getConnection(String user, char[] password, String dbURL) {
+    /**
+     * Get connection to database with <code>dbURL</code> by <code>user</code> and <code>password</code>.
+     *
+     * @param user     Username
+     * @param password Password
+     * @param dbURL    Database URL
+     * @return Connection to database
+     * @throws SQLException If something gone wrong
+     */
+    private static Connection getConnection(String user, char[] password, String dbURL) throws SQLException {
         String url = DB_PREFIX + dbURL + DB_POSTFIX;
-        Connection conn = null;
 
         try {
             Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(url, user, new String(password));
+            return DriverManager.getConnection(url, user, new String(password));
         } catch (SQLException e) {
-            System.out.println("Can't connect to current database");
+            throw e;
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            throw new SQLException("Database driver not registered");
         }
-        return conn;
     }
 
-    public static Database connect(String user, char[] password) {
-        return connect(user, password, DEFAULT_DB_URL);
+    /**
+     * Connect to database with default DB URL by <code>user</code> and <code>password</code>.
+     *
+     * @param user     Username
+     * @param password Password
+     * @return Instance of Database for working with specified database
+     * @throws SQLException If something gone wrong (DB is busy, incorrect username and password,
+     *                      not registered DB driver)
+     */
+    public static Database connect(String user, char[] password) throws SQLException {
+        try {
+            return connect(user, password, DEFAULT_DB_URL);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
-
-    public static Database connect(String user, char[] password, String dbURL) {
+    /**
+     * Connect to database with <code>dbURL</code> by <code>user</code> and <code>password</code>.
+     *
+     * @param user     Username
+     * @param password Password
+     * @param dbURL    Database URL
+     * @return Instance of Database for working with specified database
+     * @throws SQLException If something gone wrong (DB is busy, incorrect username and password,
+     *                      not registered DB driver)
+     */
+    public static Database connect(String user, char[] password, String dbURL) throws SQLException {
         Connection connection;
-        if ((connection = getConnection(user, password, dbURL)) != null) {
+        try {
+            connection = getConnection(user, password, dbURL);
+
+            createBooksTableIfNotExists(connection);
+
             return new Database(connection);
-        } else {
-            return null;
+        } catch (SQLException e) {
+            throw e;
         }
     }
 
-
-    public void addBook (String isbn){
+    public void addBook(String isbn) throws BookNotFoundException, SQLException {
         try {
             Book book = Book.request(isbn);
 
             PreparedStatement prst = connection.prepareStatement(ADD_BOOK);
-            prst.setString(1,book.getTitle());
-            prst.setString(2,book.getAuthor());
+            prst.setString(1, book.getTitle());
+            prst.setString(2, book.getAuthor());
             prst.setString(3, isbn);
             prst.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
-            prst.setInt(5,10);
+            prst.setInt(5, 10);
 
             prst.executeUpdate();
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw e;
         } catch (IOException e) {
             e.printStackTrace();
         } catch (BookNotFoundException e) {
-            e.printStackTrace();
+            throw e;
         }
-
     }
 
-    public void changeBooksRemained(String isbn, int number) {
-
+    public void changeBooksRemained(String isbn, int number) throws SQLException {
         try {
-            PreparedStatement prst = connection.prepareStatement("UPDATE BOOKS SET REMAINED_AMOUNT=" + number +" WHERE ISBN=" + isbn);
+            final String UPDATE_BOOKS_AMOUNT_BY_ISBN = "UPDATE BOOKS SET REMAINED_AMOUNT=?  WHERE ISBN=?";
+            PreparedStatement prst = connection.prepareStatement(UPDATE_BOOKS_AMOUNT_BY_ISBN);
+            prst.setInt(1, number);
+            prst.setString(2, isbn);
             prst.executeUpdate();
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw e;
         }
-
     }
 
     /**
      * Get number of books available for rent.
      *
-     * @param bookId Id of book
+     * @param isbn Book's ISBN
      * @return Number of books available for rent
      */
-    public int booksRemained(int bookId) {
+    @Deprecated
+    public int booksRemained(String isbn) {
         throw new UnsupportedOperationException();
     }
 
-    public boolean deleteBook(int id) {
+    @Deprecated
+    public boolean deleteBook(String isbn) {
         throw new UnsupportedOperationException();
     }
 
-    public Book getBook(String isbn) {
+    public Book getBook(String isbn) throws SQLException {
         Book book = new Book();
         try {
-            PreparedStatement prst = connection.prepareStatement("SELECT * FROM BOOKS WHERE ISBN=" + isbn);
-            //prst.setString(1, isbn);
-            ResultSet rs = prst.executeQuery();
+            PreparedStatement prst = connection.prepareStatement(SELECT_ALL + " WHERE ISBN=?");
 
+            prst.setString(1, isbn);
+            ResultSet rs = prst.executeQuery();
             while (rs.next()) {
                 book = new Book();
                 book.setIsbn(rs.getString(Book.ISBN_COLUMN));
@@ -138,30 +185,27 @@ public class Database implements AutoCloseable{
                 book.setRemainedAmount(rs.getInt(Book.REMAINED_AMOUNT));
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw e;
         }
+
         return book;
     }
 
     /**
-     * Get all books from database.
+     * Get all books from database sorted by <code>sortingColumn</code>.
      *
+     * @param sortingColumn Column for order
      * @return List with all stored books
      */
-    public List<Book> getBooks(String column) {
+    public List<Book> getBooks(String sortingColumn) throws SQLException {
         Book book = null;
         List<Book> books = new LinkedList<>();
 
         try {
-
-            PreparedStatement prst = connection.prepareStatement("select * from books order by "+ column);
-            //prst.setString(1,column.toUpperCase());
+            PreparedStatement prst = connection.prepareStatement(SELECT_ALL_ORDER_BY + sortingColumn);
 
             ResultSet rs = prst.executeQuery();
-
-            //System.out.println(SORT_BY_SQL);
-
-            while (rs.next()){
+            while (rs.next()) {
                 book = new Book();
                 book.setIsbn(rs.getString(Book.ISBN_COLUMN));
                 book.setAuthor(rs.getString(Book.AUTHOR_COLUMN));
@@ -171,36 +215,15 @@ public class Database implements AutoCloseable{
                 books.add(book);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw e;
         }
 
         return books;
-    }
-
-
-
-
-    /**
-     *
-     * @param connection
-     * @throws SQLException
-     */
-    public void closeConnection (Connection connection) throws SQLException {
-        if (connection == null)
-            return;
-        try {
-            connection.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
     }
 
     @Override
     public void close() throws Exception {
         if (connection != null)
             connection.close();
-    }
-
-    public void updateBooksNumber(long isbn_oca, int newNumber) {
     }
 }
--- BookLibrary/src/main/java/javacore/coop/Main.java
+++ BookLibrary/src/main/java/javacore/coop/Main.java
@@ -1,9 +1,12 @@
 package javacore.coop;
 
 import javacore.coop.model.Book;
+import javacore.coop.model.BookNotFoundException;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Program entry class without UI
@@ -13,40 +16,65 @@ public class Main {
         Database db = null;
         try {
             db = Database.connect("", "".toCharArray(), Database.DEFAULT_DB_URL);
-            final long ISBN_OCA = 1118957407L;
-            final long ISBN_PATTERNS = 9780201485370L;
+            final String ISBN_OCA = "1118957407";
+            final String ISBN_PATTERNS = "9780201485370";
             final String BOOK_NOT_FOUND_FORMAT = "Book with ISBN %d not found";
 
-            List<Long> isbns = new ArrayList<Long>() {{
+            List<String> isbns = new ArrayList<String>() {{
                 add(ISBN_OCA);
                 add(ISBN_PATTERNS);
             }};
 
-            for (Long bookISBN : isbns) {
-                db.addBook(bookISBN);
+            for (String bookISBN : isbns) {
+                try {
+                    db.addBook(bookISBN);
+                    System.out.println("Added book with ISBN " + bookISBN + " to database");
+                } catch (BookNotFoundException e) {
+                    System.out.println("Book with ISBN " + bookISBN + " not found");
+                } catch (Exception e) {
+                    System.out.println("Database already contains book with ISBN " + bookISBN);
+                }
             }
 
-            System.out.println("Books sorted by ISBN: ");
+            System.out.println("\n== Books sorted by ISBN");
             List<Book> books = db.getBooks(Book.ISBN_COLUMN);
             for (Book book : books) {
                 System.out.println(book.toString());
             }
 
-            System.out.println("Books sorted by upload date: ");
-            books = db.getBooks(Book.UPLOAD_DATE_COLUMN);
+            System.out.println("\n== Books sorted by Upload Date");
+            books = db.getBooks(Book.ADDED_DATE_COLUMN);
             for (Book book : books) {
                 System.out.println(book.toString());
             }
 
-            System.out.println("OCA book: ");
-            Book ocaBook = db.getBook(ISBN_OCA);
-            System.out.println(ocaBook.toString());
+            System.out.println("\n== Book \"OCA\"");
+            Book ocaBook;
+            try {
+                ocaBook = db.getBook(ISBN_OCA);
+                System.out.println(ocaBook.toString());
+            } catch (SQLException e) {
+                System.out.println("Can't get book with ISBN " + ISBN_OCA);
+            }
 
-            int newNumber = 15;
-            db.updateBooksNumber(ISBN_OCA, newNumber);
-            ocaBook = db.getBook(ISBN_OCA);
-            System.out.println("OCA count updated to " + newNumber);
-            System.out.println(ocaBook.toString());
+            Random random = new Random();
+            int min = 1;
+            int max = 100;
+            int newNumber = random.nextInt(max - min + 1) + min;
+            try {
+                db.changeBooksRemained(ISBN_OCA, newNumber);
+                System.out.println("\n-> \"OCA\" count updated to " + newNumber);
+            } catch (SQLException e) {
+                System.out.println("Can't change books amount by ISBN " + ISBN_OCA);
+            }
+
+            System.out.println("\n== Now Book \"OCA\"");
+            try {
+                ocaBook = db.getBook(ISBN_OCA);
+                System.out.println(ocaBook.toString());
+            } catch (SQLException e) {
+                System.out.println("Can't get book with ISBN " + ISBN_OCA);
+            }
 
         } catch (Exception e) {
             System.out.println("Error: " + e.toString());
